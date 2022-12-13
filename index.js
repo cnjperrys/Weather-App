4fa7b179beee9219c1a5b4c2991d61efea92936e
cnjperrys <116315021+cnjperrys@users.noreply.github.com>
2022-12-13 10:51:27 -0500

    Created index.html file

---
+++ index.js
@@ -0,0 +1,52 @@
+let now = new Date();
+let date = now.getDate();
+let year = now.getFullYear();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let month = months[now.getMonth()];
+
+let h3 = document.querySelector("h3");
+h3.innerHTML = `${month} ${date}, ${year} ${day} ${hours}:${minutes}pm EST`;
+
+function searchCity(city) {
+  let apiKey = "3eb4b0dca3267978aa192a5a0660c7d2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function searchSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "3eb4b0dca3267978aa192a5a0660c7d2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#degrees").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchSubmit);
