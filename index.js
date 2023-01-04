fa70df337d0616b2a9c93f8ce3fda8cf08db3b3f
cnjperrys <116315021+cnjperrys@users.noreply.github.com>
2023-01-03 23:14:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
-
-document.querySelector("#description").innerHTML =
+ document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
 
 document.querySelector("#temperature").innerHTML = Math.round(
