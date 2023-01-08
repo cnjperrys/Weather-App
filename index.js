1b8bf5f7b251ca5afc7ebfd03f97206786a2cdca
cnjperrys <116315021+cnjperrys@users.noreply.github.com>
2023-01-08 01:19:23 -0500

    .

--- index.js
+++ index.js
@@ -43,8 +43,7 @@ function searchCity(city) {
   searchCity(city);
 }
 
-function formatDay(timestamp) {
-  console.log (response.data);
+function forecastDay(timestamp) {
   let date = new Date (timestamp * 1000);
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue","Wed","Thur","Fri","Sat"];
@@ -120,8 +119,8 @@ function displayForecast(response) {
                 width="60px"
                  />
                 <div class = "weather-forecast-temp">
-                    <span class ="weather-forecast-max">${forecastDay.temp.max}°</span> 
-                    <span class="weather-forecast-min">${forecastDay.temp.min}°</span>
+                    <span class ="weather-forecast-max">${Math.round(forecastDay.temp.max)}°</span> 
+                    <span class="weather-forecast-min">${Math.round(forecastDay.temp.min)}°</span>
                     </div>
             </div>
 `;
@@ -133,7 +132,6 @@ function displayForecast(response) {
 
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "e947cb2640f1db92e6a19005bc43b435";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
